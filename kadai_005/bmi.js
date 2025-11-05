215cdbc1264fa83a992d8b8654c10a0e88626e03
syk120 <symsayaka0120@gmail.com>
2025-11-05 23:00:17 +0900

    kadai_005追加

---
+++ kadai_005/bmi.js
@@ -0,0 +1,20 @@
+let userName1;
+userName1 = '体重';
+let userNumber1;
+userNumber1 = 68;
+console.log(userName1); 
+console.log(userNumber1); 
+
+
+let userName2;
+userName2 = '身長';
+let userNumber2;
+userNumber2 = 1.7;
+console.log(userName2); 
+console.log(userNumber2); 
+
+let userName3 = '計算式';
+console.log(userName3);
+
+let bmi = 68 / (1.7*1.7);
+console.log(bmi);
